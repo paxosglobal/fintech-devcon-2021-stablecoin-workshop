a9714380c438fd7a21a638debbd702dfaedc8c35
Brent <bmperrea@gmail.com>
2021-08-27 12:30:04 -0500

    update reserve and minted balances

--- pkg/server/withdrawal.go
+++ pkg/server/withdrawal.go
@@ -17,7 +17,9 @@ func (s *Server) CreateWithdrawal(req *Withdrawal) (*Activity, error) {
 	}
 	s.mu.Lock()
 	s.activities = append(s.activities, newActivity)
-	s.balances.UsdOnPlatform = s.balances.UsdOnPlatform.Sub(newActivity.Amount)
+	s.balances.UsdOnPlatform = s.balances.UsdOnPlatform.Sub(newActivity.Amount) // debut funds from on-platform holdings
+	s.balances.UsdInReserve = s.balances.UsdInReserve.Sub(newActivity.Amount)   // put funds in reserve
+	s.balances.UsdkMinted = s.balances.UsdkMinted.Add(newActivity.Amount)       // mark as minted on chain
 	s.mu.Unlock()
 
 	// TODO: implement kicking off an actual blockchain withdrawal
