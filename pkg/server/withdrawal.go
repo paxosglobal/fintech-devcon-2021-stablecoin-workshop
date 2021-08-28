feb0fb9e17f9476ee92f15f04b2e743df8e848d8
Brent <bmperrea@gmail.com>
2021-08-27 21:24:01 -0500

    Update withdrawal.go

--- pkg/server/withdrawal.go
+++ pkg/server/withdrawal.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -17,8 +18,11 @@ func (s *Server) CreateWithdrawal(req *Withdrawal) (*Activity, error) {
 	}
 	s.mu.Lock()
 	s.activities = append(s.activities, newActivity)
+	if s.balances.UsdOnPlatform.LessThan(newActivity.Amount) {
+		return nil, fmt.Errorf("not enough funds")
+	}
 	s.balances.UsdOnPlatform = s.balances.UsdOnPlatform.Sub(newActivity.Amount) // debut funds from on-platform holdings
-	s.balances.UsdInReserve = s.balances.UsdInReserve.Sub(newActivity.Amount)   // put funds in reserve
+	s.balances.UsdInReserve = s.balances.UsdInReserve.Add(newActivity.Amount)   // put funds in reserve
 	s.balances.UsdkMinted = s.balances.UsdkMinted.Add(newActivity.Amount)       // mark as minted on chain
 	s.mu.Unlock()
 
