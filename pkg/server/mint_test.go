4d847416aeee3b2c893fa8a6e0eb73a83bdebb8a
Brent <bmperrea@gmail.com>
2021-09-02 10:20:32 -0500

    add a balance assertion to mint_test

--- pkg/server/mint_test.go
+++ pkg/server/mint_test.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
 	contracts "github.com/paxosglobal/fintech-devcon-2021-stablecoin-workshop/build"
 	"github.com/shopspring/decimal"
@@ -22,12 +24,20 @@ func TestMint(t *testing.T) {
 		balances:   Balances{},
 		activities: []Activity{},
 	}
-	contractAddr, _, _, err := contracts.DeployUSDK(OwnerTransactor, sim)
+	contractAddr, _, usdkBindings, err := contracts.DeployUSDK(OwnerTransactor, sim)
 	require.NoError(t, err)
 	require.Equal(t, contractAddr.String(), ContractAddr)
 	sim.Commit() // write a block to the blockchain
 
-	err = s.mint("0xc8AaE7A85CD634C0808C4aCb250B99DD7980483B", decimal.RequireFromString("1.5"))
+	destination := "0xc8AaE7A85CD634C0808C4aCb250B99DD7980483B"
+	amount := decimal.RequireFromString("1.5")
+	err = s.mint(destination, amount)
 	require.NoError(t, err)
 	sim.Commit() // write a block to the blockchain
+
+	options := (*bind.CallOpts)(nil)
+	endBalance, err := usdkBindings.BalanceOf(options, common.HexToAddress(destination))
+	require.NoError(t, err)
+	actual := decimal.NewFromBigInt(endBalance, 0).Div(DecimalToInt)
+	require.Equal(t, amount.String(), actual.String(), "make sure to sign and broadcast a mint transaction")
 }
