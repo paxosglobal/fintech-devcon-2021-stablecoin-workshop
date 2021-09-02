92795ec6034acded99ca5fd4950055d8acd915d0
ilan gitter <8359193+gitteri@users.noreply.github.com>
2021-09-02 10:03:51 -0600

    exercise-2

--- pkg/server/mint.go
+++ pkg/server/mint.go
@@ -1,11 +1,8 @@
 package server
 
 import (
-	"context"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	contracts "github.com/paxosglobal/fintech-devcon-2021-stablecoin-workshop/build"
 	"github.com/shopspring/decimal"
@@ -58,76 +55,6 @@ func (s *Server) mint(destination string, amount decimal.Decimal) error {
 }
 
 func (s *Server) mintWithBindings(destination string, amount decimal.Decimal) error {
-	usdkBindings, err := contracts.NewUSDK(common.HexToAddress(ContractAddr), s.ethClient)
-	if err != nil {
-		return err
-	}
-	_, err = usdkBindings.Mint(OwnerTransactor, common.HexToAddress(destination), amount.Mul(DecimalToInt).BigInt())
-	return err
-}
-
-func (s *Server) mintWithExplicitSigning(destination string, amount decimal.Decimal) error {
-	ctx := context.Background()
-	x, err := s.createMintTransaction(ctx, destination, amount)
-	if err != nil {
-		return err
-	}
-	signedTx, err := signTransaction(OwnerTransactor, x)
-	if err != nil {
-		return err
-	}
-	err = s.Broadcast(ctx, signedTx)
-	if err != nil {
-		return err
-	}
+	// TODO: fill out this function for exercise 2!
 	return nil
-}
-
-func (s *Server) createMintTransaction(ctx context.Context, destination string, amount decimal.Decimal) (*types.Transaction, error) {
-	data, err := createMintTransactionData(destination, amount)
-	if err != nil {
-		return nil, err
-	}
-	contractAddress := common.HexToAddress(ContractAddr)
-	latestBlockFilter := (*big.Int)(nil)
-	// rpc call to read the blockchain node to get the next nonce to use
-	currentNonce, err := s.ethClient.NonceAt(ctx, OwnerTransactor.From, latestBlockFilter)
-	if err != nil {
-		return nil, err
-	}
-	// rpc call to read the blockchain node to get the current base fee
-	header, err := s.ethClient.HeaderByNumber(ctx, latestBlockFilter)
-	if err != nil {
-		return nil, err
-	}
-	// rpc call to read the blockchain node to get a suggested tip cap based on recent tip caps on previous blocks
-	suggestedTipCap, err := s.ethClient.SuggestGasTipCap(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return types.NewTx(&types.DynamicFeeTx{
-		ChainID:   ChainIDLocal,
-		Nonce:     currentNonce,
-		GasFeeCap: (&big.Int{}).Add(header.BaseFee, suggestedTipCap),
-		GasTipCap: suggestedTipCap,          // 1 gwei isn't very much in real life, you'll likely pay more
-		Gas:       OwnerTransactor.GasLimit, // gas limit - an upper bound on how much compute your transaction will use
-		To:        &contractAddress,
-		Value:     big.NewInt(0), // amount of Eth being transferred
-		Data:      data,          // the smart contract call
-	}), nil
-}
-
-func createMintTransactionData(destination string, amount decimal.Decimal) ([]byte, error) {
-	addr := common.HexToAddress(destination)
-	weiInt := amount.Mul(DecimalToInt).BigInt()
-	method := "mint"
-	return UsdkABI.Pack(method, addr, weiInt)
-}
-
-func signTransaction(keyedTransactor *bind.TransactOpts, x *types.Transaction) (*types.Transaction, error) {
-	return keyedTransactor.Signer(keyedTransactor.From, x)
-}
-
-func (s *Server) Broadcast(ctx context.Context, x *types.Transaction) error {
-	return s.ethClient.SendTransaction(ctx, x)
 }
